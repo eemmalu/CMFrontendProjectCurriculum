6113543a776d237b406bad7255acd73b7712accc
Emma Lu <emmalu.2410@gmail.com>
2025-10-26 11:14:50 -0700

    Complete Javascript Exercise

--- module-02-html-css-javascript/exercises/03-first-javascript/script.js
+++ module-02-html-css-javascript/exercises/03-first-javascript/script.js
@@ -1,32 +1,24 @@
+console.log("Hello world");
+
 // Button Click Counter Exercise
 // Your goal: Make the counter increase each time the button is clicked!
 
 // TODO: Create a variable to store the click count (start at 0)
-
+let count = 0;
 
 // TODO: Select the button element using its ID
 // Hint: Use document.getElementById('click-button')
-
+let button = document.getElementById('click-button');
 
 // TODO: Select the display element where the count will be shown
 // Hint: Use document.getElementById('count-display')
-
+let display = document.getElementById('count-display');
 
 // TODO: Add an event listener to the button that listens for 'click' events
 // Hint: button.addEventListener('click', function() { ... })
+button.addEventListener('click', function(){
+    count++;
+    display.textContent = "Clicks: " + count;
+    display.textContent = `Clicks: ${count}`;
+});
 
-
-    // TODO: Inside the click handler function:
-
-    // TODO: Increase the click count by 1
-
-
-    // TODO: Update the text content of the display element
-    // Hint: display.textContent = 'Clicks: ' + clickCount;
-    // Or use template literals: display.textContent = `Clicks: ${clickCount}`;
-
-
-
-
-// BONUS: Try console.log() to see if your code is running!
-// console.log('Script loaded!');
